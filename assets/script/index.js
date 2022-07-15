ac3c15900cdf97f6917b90d3f4d512e0b2754473
Leonardo Zamora Leiva <leonardo.zamoral@sansano.usm.cl>
2022-07-14 22:16:29 -0400

    lista funcion de nombre

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
   $("table").DataTable();
 });
+
+// function agregarNombre() {
+//   let nombreUsuario = document.getElementById("#nombreUsuario").value;
+//   alert(nombreUsuario);
+// }
+
+function preguntarNombre() {
+  var name = prompt("Dinos tu nombre: ");
+  if (name == null || name == "") {
+    txt = "No Name provided";
+  } else {
+    txt = `Hello ${name}!`;
+  }
+  alert(txt);
+  if (txt != "No Name provided") {
+    // var name = document.getElementById("nombreUsuario");
+    var paragraph = document.getElementById("nombreUsuario");
+    var text = document.createTextNode(`Bienvenido ${name}`);
+    paragraph.classList.add("nameStyle");
+    paragraph.appendChild(text);
+  }
+}
+
+function agregarNombre() {}
+
+// // agregarNombre();
+
+// function carritoPopUp() {
+//   var popup = document.getElementById("myPopup");
+//   if (popup.classList.contains("show")) {
+//     popup.classList.remove("show");
+//   } else {
+//     popup.classList.add("show");
+//   }
+// }
+
+// var paragraph = document.getElementById("p");
+// var text = document.createTextNode("This just got added");
+
+// paragraph.appendChild(text);
