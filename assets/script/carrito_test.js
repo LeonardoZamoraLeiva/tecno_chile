c10be289fc88c47aa76d1d2122830d05211f0c84
Leonardo Zamora Leiva <leonardo.zamoral@sansano.usm.cl>
2022-07-20 20:10:47 -0400

    agregado datatables desde array de objetos

--- assets/script/carrito_test.js
+++ assets/script/carrito_test.js
@@ -2,62 +2,62 @@ let productos = [
   {
     imagen:
       "https://www.paris.cl/dw/image/v2/BCHW_PRD/on/demandware.static/-/Sites-paris-marketplace/default/dw9d638f5d/images/imagenes-productos/800/MKX5/MKX53ATHYZ-001.jpg?sw=513&sh=654&sm=fit",
-    nombre: "Nintendo Switch V2",
-    categoria: "Consola",
-    precio: 319.99,
+    Nombre: "Nintendo Switch V2",
+    Categoria: "Consolas",
+    Precio: 319.99,
     Codigo: "CS-8456",
-    stock: true,
+    Stock: "Si",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.",
   },
   {
     imagen:
       "https://www.lapolar.cl/dw/image/v2/BCPP_PRD/on/demandware.static/-/Sites-master-catalog/default/dw37c35450/images/large/24206726.jpg?sw=1200&sh=1200&sm=fit",
-    nombre: "Samsung Galaxy AE32 128GB",
-    categoria: "Telefonía",
-    precio: 159.99,
+    Nombre: "Samsung Galaxy AE32 128GB",
+    Categoria: "Telefonía",
+    Precio: 159.99,
     Codigo: "SP-9889",
-    stock: true,
+    Stock: "Si",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.",
   },
   {
     imagen:
       "https://media.ldlc.com/r1600/ld/products/00/05/94/60/LD0005946049.jpg",
-    nombre: "Xiamoi Redmi 10C 64GB",
-    categoria: "Telefonía",
-    precio: 139.99,
+    Nombre: "Xiamoi Redmi 10C 64GB",
+    Categoria: "Telefonía",
+    Precio: 139.99,
     Codigo: "SP-5468",
-    stock: true,
+    Stock: "Si",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.",
   },
   {
     imagen:
       "https://sipoonline.cl/wp-content/uploads/2022/01/Consola-Sony-PlayStation-5-Blu-Ray-Glacier-White.png",
-    nombre: "PlayStation 5 Glacier White",
-    categoria: "Consolas",
-    precio: 679.99,
+    Nombre: "PlayStation 5 Glacier White",
+    Categoria: "Consolas",
+    Precio: 679.99,
     Codigo: "CS-4568",
-    stock: false,
+    Stock: "No",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.",
   },
   {
     imagen:
       "https://http2.mlstatic.com/D_NQ_NP_628105-MLA49423179604_032022-O.jpg",
-    nombre: "Xiaomi Poco X4 Pro 128GB",
-    categoria: "Telefonia",
-    precio: 309.99,
+    Nombre: "Xiaomi Poco X4 Pro 128GB",
+    Categoria: "Telefonia",
+    Precio: 309.99,
     Codigo: "SP-45231",
-    stock: false,
+    Stock: "No",
     descripcion:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.",
   },
 ];
 
-const divisa = "$";
-let productosCarrito = document.querySelector("#carritoElements");
+// const divisa = "$";
+let productosCarrito = document.getElementById("carritoElements");
 
 function crearProductos() {
   productos.forEach((info) => {
@@ -78,7 +78,7 @@ function crearProductos() {
     // Precio
     const miNodoPrecio = document.createElement("p");
     miNodoPrecio.classList.add("card-text");
-    miNodoPrecio.textContent = `${info.precio}${divisa}`;
+    miNodoPrecio.textContent = `${info.precio}`;
     // Boton
     const miNodoBoton = document.createElement("button");
     miNodoBoton.classList.add("btn", "btn-primary");
@@ -96,3 +96,16 @@ function crearProductos() {
 }
 
 crearProductos();
+
+$(document).ready(function () {
+  $("#example").DataTable({
+    data: productos,
+    columns: [
+      { data: "Nombre" },
+      { data: "Categoria" },
+      { data: "Precio" },
+      { data: "Codigo" },
+      { data: "Stock" },
+    ],
+  });
+});
