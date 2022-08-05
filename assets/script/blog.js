3ea8821b10dfbff27caeed80ff884fdbe9675d05
Nico <inath.zp@gmail.com>
2022-08-04 20:57:03 -0400

    Loading screen

--- assets/script/blog.js
+++ assets/script/blog.js
@@ -16,3 +16,14 @@ function soloLetras(e){
         return false;
     }
 }
+
+var myVar;
+
+function tiempo() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myCards").style.display = "block";
+}
